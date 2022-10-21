b33b5f7494067140729f404bb649201ba70ed2c6
IevaSta <ieva@kontraforma.lt>
2022-10-21 16:56:51 +0300

    variables

---
+++ js/01-variables.js
@@ -0,0 +1,33 @@
+// komentaras
+
+/* keliu eiluciu komentai */
+
+/* 
+KINTAMIEJI
+
+Iniciavimo budas:
+const - default iniciavimo budas
+let - antrinis budas inicijuoti ir reiksme galim keisti
+var - seniausias, nenaudoti saugumo sumetimais
+
+Duomenu tipas:
+number
+string
+boolean (logine reiksme: true | false)
+array (masyvas, sarasas) []
+
+
+
+*/
+
+const vardas = 'Petras';
+const pasisveikinimas = 'Laba diena, ' + vardas + '.';
+console.log(pasisveikinimas);
+
+const ar = true;
+const ar2 = false;
+
+console.log('ar: ', ar, 'ar2: ', ar2);
+
+const masyvas = [8, 1, 5, 6, 5];
+console.log('masyvas: ', masyvas);
