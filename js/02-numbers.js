16120584e52aedd375960daca6db43222dfb83f9
IevaSta <ieva@kontraforma.lt>
2022-10-21 17:13:03 +0300

    Numbers

---
+++ js/02-numbers.js
@@ -0,0 +1,22 @@
+/*
+NUMBERS
+
+-normalus (sveikieji, desimtainiai)
+-nenormalus (NaN not a number, Infinity ir -Infinity)
+
+*/
+
+const sveikasis = 5;
+const desimtainis = 3.14;
+const neSkaicius = NaN;
+
+//^
+const a = 'a';
+const b = 'b';
+console.log(a/b);
+
+
+const begalybe = Infinity;
+
+//^
+console.log(2/false);
