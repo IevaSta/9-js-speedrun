4f86014831e0064fd272e178e710b9af3f8f212a
IevaSta <ieva@kontraforma.lt>
2022-10-21 17:36:26 +0300

    Operators

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -4,6 +4,10 @@ NUMBERS
 -normalus (sveikieji, desimtainiai)
 -nenormalus (NaN not a number, Infinity ir -Infinity)
 
+OPERATORIAI
+++ pakeliam vienu vienetu
+-- sumazinam vienu vienetu
++= savo paties reiksmes pakeitimas
 */
 
 const sveikasis = 5;
@@ -20,3 +24,16 @@ const begalybe = Infinity;
 
 //^
 console.log(2/false);
+
+//kelti laipsniu **
+console.log(2**4);
+
+//liekana %
+console.log(4%3);
+
+//+= 
+let d = 4;
+const e = 2;
+
+d += e;
+console.log(d);
