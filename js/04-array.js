67d634ca2abbcf9f1cd11b31e6d581d948f23405
IevaSta <ieva@kontraforma.lt>
2022-10-21 18:27:51 +0300

    Array

---
+++ js/04-array.js
@@ -0,0 +1,31 @@
+/*
+ARRAY
+-pozicijos prasideda nuliu
+-pozicijos tik sveikieji skaiciai
+
+-ilgis .lenth
+
+*/
+
+
+const pazymiai = [2,5,9,4,7];
+console.log('Pazymiai:', pazymiai);
+
+//koks pirmas
+console.log(pazymiai[0]);
+
+//koks paskutinis
+const pazymiuKiekis = pazymiai.length;
+const paskutinioPozicija = pazymiuKiekis -1;
+
+console.log(pazymiai[paskutinioPozicija]);
+console.log(pazymiai[pazymiai.length -1]);
+
+//vidurkis
+
+
+//kiekis
+
+
+//maziausias - not this time
+//didziausias - not this time
