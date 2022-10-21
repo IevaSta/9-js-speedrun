b956ef0668f9941ebe14e53d6b7c13a41f337bc4
IevaSta <ieva@kontraforma.lt>
2022-10-21 19:00:49 +0300

    if

---
+++ js/05-if.js
@@ -0,0 +1,23 @@
+/*
+if
+
+Sablonai:
+if () {}
+if () {} else {}
+
+Palyginimo operatoriai:
+- visi >, <, <=, >=, ==, !=, ===, !==
+- naudotini >, <, <=, >=, ===, !==
+- NEnaudotini ==, !=  (patikrina reiksme, nepatikrina tipo)
+
+*/
+
+const a =77;
+const b=55;
+
+if (a>b) {
+console.log('A yra daugiau uz b');
+} else {
+console.log('A nera daugiau uz b');
+}
+
