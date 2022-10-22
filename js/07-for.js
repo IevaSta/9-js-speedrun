bedef813e762951fb33a412ba76ce973438b8aa5
IevaSta <ieva@kontraforma.lt>
2022-10-22 13:39:13 +0300

    Loops

--- js/07-for.js
+++ js/07-for.js
@@ -1,12 +1,12 @@
 /*
 for - ciklas/loop
 
-loop: for, while, for-in, for-of, foreach...
-
+loop: for, while, for-of, foreach...
+object loop: for-in
 */
 
 const pazymiai = [10, 2];
-let suma = 0;
+
 
 /*
 ciklas prasideda su reiksme nulis: let i=0;
@@ -14,11 +14,58 @@ nurodome kiek kartu turi suktis: i<5;
 kokiu zingsniu daryti cikla: i++;
 */
 
+//FOR
+let sumaFor = 0;
 for (let i=0; i< pazymiai.length; i++) {
 
     const pazymys = pazymiai [i];
-    suma += pazymys;
+    sumaFor += pazymys;
 }
 
-const vidurkis = suma / pazymiai.length;
-console.log(vidurkis);
+const vidurkisFor = sumaFor / pazymiai.length;
+console.log(vidurkisFor);
+
+//WHILE
+
+/* daryk tol, kol tenkinsi salyga i<5
+*/
+
+let sumaWhile = 0;
+let i2 = 0;
+
+while (i2 < pazymiai.length) {
+    const pazymys = pazymiai [i2];
+    sumaWhile += pazymys;
+    i2++
+}
+
+const vidurkisWhile = sumaWhile / pazymiai.length;
+console.log(vidurkisWhile);
+
+//FOR OF
+/* jei darbas vyksta ne su tusciu masyvu
+*/
+
+let sumaForOf = 0;
+
+for (const pazymys of pazymiai) {
+    sumaForOf+= pazymys;
+}
+
+const vidurkisForOf = sumaForOf / pazymiai.length;
+console.log(vidurkisForOf);
+
+
+//FOR EACH - leciausias is visu ciklu
+/* sintakse: .forEach(() => {})
+() parametrai: reiksmes, indeksai (pozicijos), masyvas
+*/
+
+let sumaForEach = 0;
+pazymiai.forEach((a, b) => {
+sumaForEach += a;
+});
+
+const vidurkisForEach = sumaForEach / pazymiai.length;
+console.log(vidurkisForEach);
+
