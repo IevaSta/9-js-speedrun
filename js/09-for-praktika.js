a96ba53a2e27939d57db669d94aedaefd2432fd9
IevaSta <ieva@kontraforma.lt>
2022-10-24 22:03:41 +0300

    function praktika - didziausias skaicius;

--- js/09-for-praktika.js
+++ js/09-for-praktika.js
@@ -8,87 +8,90 @@ Suskaiciuoti, ka gausime susumavus skaicius intervale tarp (imtinai):
 -70...30
 */
 
-function sumaIntervale (nuo, iki) {
+function sumaIntervale(nuo, iki) {
     let suma = 0;
-    for (let i=nuo; i<=iki; i++) {
+    for (let i = nuo; i <= iki; i++) {
         suma += i;
-        }
-        return suma;
+    }
+    return suma;
 }
- console.log(sumaIntervale (0, 1));
+console.log(sumaIntervale(0, 1));
+console.log('----------------');
 
 
 
 
 
-
- /*
- panaudojant cikla perrasiti tekstinio tipo kintamuju reiksmes is kito galo
- */
+/*
+panaudojant cikla perrasiti tekstinio tipo kintamuju reiksmes is kito galo
+*/
 
 
 //SUVOKIMUI
-function reverseString (text) {
-    
-    let reverse = '';
-    const indeksas = text.length -1;
-    
-for (let i=0; i<text.length; i++) {
-    const positionReverse = indeksas - i;
-    const letterReverse = text[positionReverse]
-    reverse += letterReverse;
-}
+function reverseString(text) {
 
-return reverse;
+    let reverse = '';
+    const indeksas = text.length - 1;
+
+    for (let i = 0; i < text.length; i++) {
+        const positionReverse = indeksas - i;
+        const letterReverse = text[positionReverse]
+        reverse += letterReverse;
+    }
+
+    return reverse;
 }
 
 console.log(reverseString('abcdef'));
+console.log('----------------');
 
 //SPRENDIMAS nr 1
 
-function reverseString2 (text) {
-    
-    let reverse = '';
-    
-for (let i=0; i<text.length; i++) {
-    reverse = text[i] + reverse;
-}
+function reverseString2(text) {
 
-return reverse;
+    let reverse = '';
+
+    for (let i = 0; i < text.length; i++) {
+        reverse = text[i] + reverse;
+    }
+
+    return reverse;
 }
 
 console.log(reverseString2('abcdef'));
+console.log('----------------');
 
 //SPRENDIMAS nr 2 (nuo iki panaudojamas)
 
-function reverseString3 (text) {
-    
-    let reverse = '';
-    
-for (let i=text.length-1; i>=0; i--) {
-    reverse += text[i];
-}
+function reverseString3(text) {
 
-return reverse;
+    let reverse = '';
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        reverse += text[i];
+    }
+
+    return reverse;
 }
 
 console.log(reverseString3('abcdef'));
+console.log('----------------');
 
 
 
 
 
 
- /*
- suskaiciuoti, kiek nurodytame intervale yra skaiciu, kurie dalijasi be liekanos is 3, 5 ir 7 atskirai:
- 0-11
+/*
+suskaiciuoti, kiek nurodytame intervale yra skaiciu, kurie dalijasi be liekanos is 3, 5 ir 7 atskirai:
+0-11
 
- rezulata pateikti tokiu formatu:
- -Skaičių intervale tarp a ir b, besidalijančių be liekanos iš c yra x vienetai.
- */
+rezulata pateikti tokiu formatu:
+-Skaičių intervale tarp a ir b, besidalijančių be liekanos iš c yra x vienetai.
+*/
 
 
- function liekana (nuo, iki, daliklis) {
+function liekana(nuo, iki, daliklis) {
 
     let count = 0;
 
@@ -96,11 +99,11 @@ console.log(reverseString3('abcdef'));
         if (i % daliklis === 0) {
             count++;
         }
-}
- 
-return `Skaičių intervale tarp ${nuo} ir ${iki}, besidalijančių be liekanos iš ${daliklis} yra ${count} vienetai.`;
+    }
 
- }
- console.log(liekana (0,11,3));
- console.log(liekana (0,11,5));
- console.log(liekana (0,11,7));
+    return `Skaičių intervale tarp ${nuo} ir ${iki}, besidalijančių be liekanos iš ${daliklis} yra ${count} vienetai.`;
+
+}
+console.log(liekana(0, 11, 3));
+console.log(liekana(0, 11, 5));
+console.log(liekana(0, 11, 7));
--- js/10-function-praktika.js
+++ js/10-function-praktika.js
@@ -3,8 +3,9 @@ function tusciaFunkcija() {
 }
 
 console.log(tusciaFunkcija());
+console.log('----------------');
 
-/*su testais, kurie leistu dauginti tik skaicius */
+/*su testais, kurie leistu dauginti TIK SKAICIUS */
 
 function daugyba(a, b) {
     if (typeof a !== 'number') {
@@ -27,9 +28,71 @@ function daugyba(a, b) {
 }
 
 console.log('skaiciai:', daugyba(3, 8));
-console.log('skaicius ir string:', daugyba(7, 'string'));
-console.log('skaicius ir tuscias string:', daugyba(7, ''));
-console.log('skaicius ir boolean:', daugyba(7, true));
-console.log('booleanai:', daugyba(false, true));
-console.log('skaicius ir objektas tuscias:', daugyba(7, []));
-console.log('skaicius ir objektas netuscias:', daugyba(7, [1, 2, 3]));
+// console.log('skaicius ir string:', daugyba(7, 'string'));
+// console.log('skaicius ir tuscias string:', daugyba(7, ''));
+// console.log('skaicius ir boolean:', daugyba(7, true));
+// console.log('booleanai:', daugyba(false, true));
+// console.log('skaicius ir objektas tuscias:', daugyba(7, []));
+// console.log('skaicius ir objektas netuscias:', daugyba(7, [1, 2, 3]));
+console.log('----------------');
+
+
+/*SKAITMENU KIEKIS*/
+
+function numSize(num) {
+    if (typeof num !== 'number'
+        //|| isNaN(num) - nereikalingas, nes prigauna isFinite
+        || !isFinite(num)) { //ne baigtinis
+        return 'ERROR: netinkamas tipas.'
+    }
+
+
+    const numAsString = '' + num;
+    let size = numAsString.length;
+
+    if (num % 1 !== 0) {
+        size--;
+    }
+
+    if (num < 0) {
+        size--;
+    }
+
+    return size;
+}
+
+console.log(numSize('asd'));
+console.log(numSize(NaN));
+console.log(numSize(Infinity));
+console.log(numSize(12345));
+console.log(numSize(3.14));
+console.log('----------------');
+
+
+
+/*DIDZIAUSIAS SKAICIUS*/
+function bigNum(list) {
+    if (typeof list !== 'object') {
+        return 'ERROR: netinkamas tipas'
+    }
+    if (list.length === 0) {
+        return 'ERROR: sarasas negali buti tuscias'
+    }
+
+    let biggestNumber = list[0];
+
+    for (let i = 0; i < list.length; i++) {
+        const number = list[i];
+        if (number > biggestNumber) {
+            biggestNumber = number;
+        }
+    }
+
+
+    return biggestNumber;
+}
+
+console.log(bigNum([1, 23, 4]));
+console.log(bigNum([-1, -23, -4]));
+console.log(bigNum([]));
+console.log(bigNum('testas'));
